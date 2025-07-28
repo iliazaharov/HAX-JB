f90ce21ad993450e9372cb6fba8b446e9dce0e3b
Ilya.Zakharov <iliazaharov@gmail.com>
2025-07-28 15:41:16 +0200

    All files commit

--- scripts/papers.js
+++ scripts/papers.js
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+// <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8"/>
@@ -22,7 +22,7 @@
 <body>
 <h1>Published Google Doc (Wider View)</h1>
 <div id="iframe-container">
-    <iframe src="https://docs.google.com/document/d/e/2PACX-1vQ_0qDX3xjIjBoZNLZzm7wtzw-cMxnGwaB6W4AmTBBou9BoXEU2RjlTSl7D4dIxCfAajTtyxzyzeGg7/pub?embedded=true"></iframe>
+    <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ6TpxwLlvZ1WZiK2780VY-aik-yuFLnsbrL6Oqte3qe5eKpYUEot8Jsc48TheCiANQmMEMuNvHkOqu/pub?output=ods"></iframe>
 </div>
 </body>
 </html>
